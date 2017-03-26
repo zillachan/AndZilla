d6b68e2697e2925be32747808914b5ca185b0964
guanguoxiang <abcgg8@qq.com>
2017-03-26 23:26:05 +0800

    提交权限检查

---
+++ libzilla/src/main/java/ggx/com/libzilla/permission/PermissionFail.java
@@ -0,0 +1,17 @@
+package ggx.com.libzilla.permission;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author jerry.Guan
+ * @date 2016/9/18
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface PermissionFail {
+
+
+}
